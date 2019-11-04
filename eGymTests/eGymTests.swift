eb96a1f4056221d27d65ee9cb382f3079fef3c50
Vaibhav Bangde <vaibhavbangde7888@gmail.com>
2019-11-04 17:16:39 +0530

    Added test file.

--- eGymTests/eGymTests.swift
+++ eGymTests/eGymTests.swift
@@ -11,24 +11,77 @@ import XCTest
 
 class eGymTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    var interactor : TopStoriesInteractorProtocol!
+    
+    struct testTopStoriesFetchService : FetchTopStoriesServiceProtocol {
+        var testJsonData : Data
+        
+        init(data:Data) {
+            self.testJsonData = data
+        }
+        
+        func fetchTopStories(completion: @escaping (Data?) -> Void) {
+            completion(testJsonData)
         }
     }
+    
+    override func setUp() {
+        guard let jsonData = self.readJson() else { return }
 
+        let topStoriesFetchService = testTopStoriesFetchService(data: jsonData)
+        let createStoryEntityService = CreateStoryEntitiesService()
+        let fetchImageUrlFromStory = FetchImageUrlFromStoryService()
+        let apiService = ApiService()
+        let fetchCellImageService = FetchCellImageService(apiService: apiService)
+        
+        self.interactor = TopStoriesInteractor(fetchTopStoriesService: topStoriesFetchService,
+                                               createStoryEntityService: createStoryEntityService,
+                                               fetchImageUrlFromStory: fetchImageUrlFromStory,
+                                               fetchCellImageService: fetchCellImageService)
+    }
+
+    func readJson() -> Data? {
+        guard let pathString = Bundle(for: type(of: self)).path(forResource: "topStories_test", ofType: "json") else {
+            print("topStories_test.json not found")
+            return nil
+        }
+
+        guard let jsonString = try? String(contentsOfFile: pathString, encoding: .utf8) else {
+            print("Unable to convert topStories_test.json to String")
+            return nil
+        }
+
+        guard let jsonData = jsonString.data(using: .utf8) else {
+            print("Unable to convert topStories_test.json to Data")
+            return nil
+        }
+        
+        return jsonData
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        self.interactor = nil
+    }
+
+    func testJsonParcing() {
+        let testExpectation = expectation(description: #function)
+        
+        self.interactor.fetchTopStories { (response) in
+            guard let stories = response else {
+                XCTAssert(false)
+                return
+            }
+            XCTAssertEqual(stories.count, 35)
+            
+            if let story = stories.first(where: { $0.title == "The Money Farmers: How Oligarchs and Populists Milk the E.U. for Millions" }) {
+                
+                XCTAssertEqual(story.byline, "By SELAM GEBREKIDAN, MATT APUZZO and BENJAMIN NOVAK")
+            }
+            
+            testExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
 }
